a0bcc86d162483ef5474c9fcdeea57278eb9cd57
Aurora Nou <auroranou@gmail.com>
2018-06-19 15:23:21 -0400

    add raw-loader to handle loading .glsl file content as strings

--- webpack.config.js
+++ webpack.config.js
@@ -13,18 +13,25 @@ module.exports = {
     modules: [path.resolve(__dirname, 'node_modules')]
   },
   module: {
-    rules: [{
-      // Include ts, tsx, and js files.
-      test: /\.(tsx?)|(js)$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          '@babel/preset-env',
-          '@babel/preset-typescript'
-        ],
+    rules: [
+      {
+        // Include ts, tsx, and js files.
+        test: /\.(tsx?)|(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-typescript'
+          ],
+        },
       },
-    }],
+      {
+        // Load .glsl shader files as strings
+        test: /\.glsl$/,
+        loader: 'raw-loader'
+      }
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
